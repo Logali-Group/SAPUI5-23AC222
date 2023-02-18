b5f542ffdf9a10f48dfaf6bf4fc4de32c5a989dc
Logali-Group <training@logaligroup.com>
2023-02-18 15:55:18 +0000

    First commit to 04_Elementos_Controles_UI5

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,8 +8,26 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("employees.controller.App", {
+
             onInit: function () {
 
+            },
+
+            onValidate: function (oEvent) {
+                console.log(oEvent.getParameters());
+                //this.byId("inputEmployee");
+                let sValue = oEvent.getParameter("newValue"),
+                    oInput = oEvent.getSource();
+                console.log(oInput);
+                if (sValue.length === oInput.getMaxLength()) {
+                    //oInput.setDescription("Ok");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("selectCountry").setVisible(true);
+                } else {
+                    //oInput.setDescription("Not Ok");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("selectCountry").setVisible(false);
+                }
             }
         });
     });
